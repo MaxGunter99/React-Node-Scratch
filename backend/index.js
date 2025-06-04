2f6e03fef46db860ef6e68c34806d4b8c0d97d52
Max Gunter <maxgunter99@gmail.com>
2025-06-04 14:10:55 -0500

    adding backend boilerplate

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app =
+	express();
+const PORT = 3001;
+
+app.use(
+	express.json()
+); // parse JSON bodies
+
+app.get(
+	'/',
+	(req, res) => {
+		res.send(
+			'Welcome to your badass backend!'
+		);
+	}
+);
+
+app.listen(
+	PORT,
+	() => {
+		console.log(
+			`Server listening on http://localhost:${PORT} :D`
+		);
+	}
+);
