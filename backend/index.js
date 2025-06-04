e82594c9aae86a28a26ac207d6462878d30b0832
Max Gunter <maxgunter99@gmail.com>
2025-06-04 14:14:12 -0500

    updating prettier format

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,13 @@
 const express = require('express');
-const app =
-	express();
+const app = express();
 const PORT = 3001;
 
-app.use(
-	express.json()
-); // parse JSON bodies
+app.use(express.json()); // parse JSON bodies
 
-app.get(
-	'/',
-	(req, res) => {
-		res.send(
-			'Welcome to your badass backend!'
-		);
-	}
-);
+app.get('/', (req, res) => {
+	res.send('Welcome to your badass backend!');
+});
 
-app.listen(
-	PORT,
-	() => {
-		console.log(
-			`Server listening on http://localhost:${PORT} :D`
-		);
-	}
-);
+app.listen(PORT, () => {
+	console.log(`Server listening on http://localhost:${PORT} :D`);
+});
