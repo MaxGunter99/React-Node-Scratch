5b502fcec56af16fad972a2a2499579e64c19dde
Max Gunter <maxgunter99@gmail.com>
2025-06-07 16:50:13 -0500

    adding footer and stamp

--- frontend/src/Routing.js
+++ frontend/src/Routing.js
@@ -9,10 +9,14 @@ import Home from "./components/home.js";
 import PageNotFound from "./components/pageNotFound.js";
 import Weather from "./components/weather.js";
 import config from "./config.js";
+// import Logo from "./images/FavIconTransparent.PNG"
+import Logo from "./images/FavIcon.png"
+import LambdaStamp from "./images/LambdaStamp.png"
 
 export default function AppRouting() {
 	return (
 		<div className="app-routing">
+
 			<header className="app-header">
 				<h1 className="app-title">{config["APP_NAME"]}</h1>
 				<nav className="nav-bar">
@@ -39,6 +43,26 @@ export default function AppRouting() {
 					<Route path="*" element={<PageNotFound />} />
 				</Routes>
 			</div>
+
+			<footer className="app-footer">
+				<div>
+					<p><strong>Michael (Max) Gunter</strong></p>
+					<p><strong>Bloom Institute of Technology</strong></p>
+					<p><strong>{config["APP_NAME"]} est. 2025</strong></p>
+				</div>
+
+				<img className="footer-logo" src={LambdaStamp} />
+				{/* <img className="footer-logo" src={Logo} /> */}
+
+				<div>
+					<a href="https://github.com/yourusername" target="_blank" rel="noreferrer">GitHub</a>
+					<br/>
+					<a href="https://linkedin.com/in/yourusername" target="_blank" rel="noreferrer">LinkedIn</a>
+					<br/>
+					<a href="https://yourportfolio.com" target="_blank" rel="noreferrer">Portfolio</a>
+				</div>
+			</footer>
+
 		</div>
 	);
 }
